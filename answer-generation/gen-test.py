a098caffc270584394f954aade3c9ebb86c4ed84
4kills <dominik.ochs@gmx.net>
2024-02-19 16:19:04 +0100

    [Dominik Ochs] Added 'retrieval chain' to stuff fetched docuuments into prompt

--- answer-generation/gen-test.py
+++ answer-generation/gen-test.py
@@ -1,11 +1,30 @@
 from langchain_community.llms import Ollama
 import subprocess
 import sys
+import ollama
 
-subprocess.run("ollama pull llama2", shell=True, text=True,  stdout=sys.stdout, stderr=sys.stderr)
-model = Ollama(model="llama2")
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
-print(model.invoke("Tell me a math joke"))
-print(model.invoke("Tell me a pyhsics joke"))
-print(model.invoke("Tell me a computer science joke"))
-print(model.invoke("Tell me a biology joke"))
+test_docs = [
+    Document(page_content="Jesse loves red but not yellow"),
+    Document(page_content = "Jamal loves green but not as much as he loves orange")
+]
+
+if 'llama' not in [model['details']['family'] for model in ollama.list()['models']]:
+    print("Pulling the model")
+    subprocess.run("ollama pull llama2", shell=True, text=True,  stdout=sys.stdout, stderr=sys.stderr)
+llm = Ollama(
+    model="llama2",
+    #temperature = 0.01,
+    repeat_penalty = 1.1,
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system", "What are everyone's favorite colors:\n\n{context}")]
+)
+chain = create_stuff_documents_chain(llm, prompt)
+
+print(chain.invoke({"context": test_docs}))
